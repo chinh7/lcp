ec69d68d7df8ec7da8cd925d0bff72bcaf446091
Đăng Nguyễn <dang.nguyen@quoine.com>
2019-09-19 17:44:03 +0700

    Revert unused code

--- main.go
+++ main.go
@@ -1,16 +1,36 @@
 package main
 
 import (
-	"os"
+	"fmt"
 
-	"github.com/QuoineFinancial/vertex/api"
+	"github.com/QuoineFinancial/vertex/db"
+	"github.com/QuoineFinancial/vertex/trie"
+	"github.com/ethereum/go-ethereum/common"
 )
 
 func main() {
-	apiServer := api.NewAPI(":5555", api.Config{
-		ChainID:     os.Getenv("CHAIN_ID"),
-		HomeDir:     os.Getenv("HOME_DIR"),
-		NodeAddress: os.Getenv("NODE_ADDRESS"),
-	})
-	apiServer.Serve()
+	db := db.NewRocksDB("data")
+
+	root := common.HexToHash("0x5991bb8c6514148a29db676a14ac506cd2cd5775ace63c30a4fe457715e9ac84")
+	tree := trie.New(root, db)
+
+	// tree := trie.New(trie.Hash{}, db)
+
+	// Update
+	tree.Update([]byte("do"), []byte("verb"))
+	tree.Update([]byte("dog"), []byte("puppy"))
+	tree.Update([]byte("doge"), []byte("coin"))
+
+	// Delete by update to nil
+	tree.Update([]byte("hors"), []byte(nil))
+
+	// Get data
+	v, _ := tree.Get([]byte("do"))
+	v, _ = tree.Get([]byte("do"))
+	v, _ = tree.Get([]byte("do"))
+	fmt.Println(string(v))
+
+	// Compute hash
+	newRootHash := tree.Hash()
+	fmt.Println(common.ToHex(newRootHash[:]))
 }
