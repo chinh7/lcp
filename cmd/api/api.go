16c895945eff7e6b23b7e1f1263fb2e3057269d3
Son Nguyen <son.nguyen.hoang@quoine.com>
2019-10-02 15:39:07 +0700

    fix tests

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -8,7 +8,6 @@ import (
 
 func main() {
 	apiServer := api.NewAPI(":5555", api.Config{
-		ChainID:     os.Getenv("CHAIN_ID"),
 		HomeDir:     os.Getenv("HOME_DIR"),
 		NodeAddress: os.Getenv("NODE_ADDRESS"),
 	})
